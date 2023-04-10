7d39f5a1cabc7f05534163843f68e8b6f7f27547
helmer <helmer@dalt.cl>
2023-04-09 21:54:28 -0400

    code refactor and context array

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import openai
 import config
+import typer
 from rich import print 
-#from rich.table import table
-
-openai.api_key = config.api_key
-print("💬 [bold green]ChatGPT API en Python[/bold green]")
+from rich.table import Table
 
 
-content = input("¿sobre que quieres hablar?")
+def main():
+    openai.api_key = config.api_key
+    
+    #Inicializamos chatGPT
+    messages=[{"role":"system","content": "Hola mundo"}]
 
-response =openai.ChatCompletion.create(model="gpt-3.5-turbo-0301", 
-                            messages=[{"role":"user","content": content}])
+    print("💬 [bold green]ChatGPT API en Python[/bold green]")
 
-print(response.choices[0].message.content)
+    table = Table("Comando", "Descripción")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear una nueva conversación")
+
+    print(table)
+
+    while True:
+        content = input("¿sobre que quieres hablar? :")
+        
+        #Se guardan Mensajes usuario
+        messages.append({"role": "user", "content": content})
+
+        if content == "exit":
+            break
+
+        response =openai.ChatCompletion.create(model="gpt-3.5-turbo-0301", 
+                                    messages=messages)
+        #Se guarda Respuestas de ChatGPt
+        messages.append({"role": "assistant", "content": response})
+
+
+        print(response.choices[0].message.content)
 
 
 
+if __name__ == "__main__":
+    typer.run(main)
